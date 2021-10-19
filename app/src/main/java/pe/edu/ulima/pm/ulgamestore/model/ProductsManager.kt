456e604ed2760f31f8db4d096c9de4e5ac98b9ff
Hernan <hernan@jsatch.com>
2021-10-19 08:51:38 -0500

    Trabajando networking low level

--- app/src/main/java/pe/edu/ulima/pm/ulgamestore/model/ProductsManager.kt
+++ app/src/main/java/pe/edu/ulima/pm/ulgamestore/model/ProductsManager.kt
@@ -1,7 +1,18 @@
 package pe.edu.ulima.pm.ulgamestore.model
 
+import pe.edu.ulima.pm.ulgamestore.network.NetworkClient
+
 class ProductsManager {
     fun getProducts() : List<Videogame> {
+
+        val networkClient = NetworkClient("")
+
+        networkClient.download({data : String ->
+            // todo ok
+        }, { error : String ->
+            // todo error
+        })
+
         val videogames = arrayListOf<Videogame>()
         videogames.add(Videogame(1, "Fortnite", 20.5f, "https://www.tonica.la/__export/1587577404181/sites/debate/img/2020/04/22/fortnite-portada_1.jpg_463833556.jpg"))
         videogames.add(Videogame(2, "Fifa 22", 40f, "https://esports.eldesmarque.com/wp-content/uploads/2021/08/FIFA-22-681x382.jpg"))
---
+++ app/src/main/java/pe/edu/ulima/pm/ulgamestore/network/NetworkClient.kt
@@ -0,0 +1,58 @@
+package pe.edu.ulima.pm.ulgamestore.network
+
+import android.os.Build
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.stream.Collectors
+
+class NetworkClient(var url : String) {
+
+    // thread-safe
+    fun download(callbackOK : (String) -> Unit, callbackError : (String) -> Unit) {
+        Thread() {
+            val urlConn = URL(url)
+            val conn = urlConn.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
+            //conn.doOutput = true
+
+            conn.connect() // cuidado!
+
+            val statusCode = conn.responseCode
+
+            if (statusCode != 200) {
+                // Error
+                callbackError("Error en la comunicacion: $statusCode")
+            }else {
+                val inputStream = conn.inputStream
+                val resp = convertInputStreamtoString(inputStream)
+                inputStream.close()
+                callbackOK(resp)
+            }
+        }.start()
+    }
+
+    fun upload() {
+
+    }
+
+    private fun convertInputStreamtoString(inputStream : InputStream) : String {
+        val br = BufferedReader(InputStreamReader(inputStream))
+        val cadena = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            br.lines().collect(Collectors.joining("\n"))
+        } else {
+            val cadenaCompleta = StringBuilder()
+            var line : String = ""
+            line = br.readLine()
+            while(line != "") {
+                cadenaCompleta.append(line).append("\n")
+                line = br.readLine()
+            }
+            cadenaCompleta.toString()
+        }
+        return cadena
+    }
+}
