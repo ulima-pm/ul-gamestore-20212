75195122bb4a75cb28085cabb6f3e07b566608a8
Hernan <hernan@jsatch.com>
2021-11-09 08:13:48 -0500

    Notificaciones locales implementadas

--- app/src/main/java/pe/edu/ulima/pm/ulgamestore/SignupActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/ulgamestore/SignupActivity.kt
@@ -1,6 +1,12 @@
 package pe.edu.ulima.pm.ulgamestore
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,12 +14,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import pe.edu.ulima.pm.ulgamestore.model.LoginManager
 
 class SignupActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        createNotificationChannel("1")
 
         findViewById<Button>(R.id.butSignup).setOnClickListener { v : View ->
             // Guardar los datos en algun servicio de persistencia
@@ -23,14 +32,10 @@ class SignupActivity : AppCompatActivity(){
                 findViewById<EditText>(R.id.eteUsername).text.toString(),
                 findViewById<EditText>(R.id.etePassword).text.toString(),
                 {
-                    val intent : Intent = Intent()
-                    val bundle = Bundle()
-                    bundle.putString("username", findViewById<EditText>(R.id.eteUsername).text.toString())
-                    bundle.putString("password", findViewById<EditText>(R.id.etePassword).text.toString())
-                    intent.putExtra("signup_data", bundle)
 
-                    setResult(RESULT_OK, intent)
-                    finish()
+
+                    sendNotification(createNotification("1"))
+
                 },
                 {
                     Log.e("SignupActivity", it)
@@ -46,5 +51,48 @@ class SignupActivity : AppCompatActivity(){
             finish()
         }
 
+    }
+
+    private fun sendNotification(notification : Notification) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        Log.d("SignupActivity", "Notification")
+        notificationManager.notify(1, notification)
+    }
+
+    private fun createNotificationChannel(channelId: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Canal 1",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotification(channelId : String) : Notification {
+        val bundle = Bundle()
+        bundle.putString("username", findViewById<EditText>(R.id.eteUsername).text.toString())
+        bundle.putString("password", findViewById<EditText>(R.id.etePassword).text.toString())
+
+        // 1010 | 0001  -> 1011
+        val intent = Intent(this, LoginActivity::class.java).apply {
+            this.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            this.putExtra("signup_data", bundle)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        return NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(android.R.drawable.stat_notify_chat)
+            .setContentTitle("Signup")
+            .setContentText("Se registro un nuevo usuario")
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH).build()
     }
 }
