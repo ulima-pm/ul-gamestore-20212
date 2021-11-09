458d5511e029635926833036c207f40c6f4026a9
Hernan <hernan@jsatch.com>
2021-11-09 09:38:22 -0500

    Obteniendo ultima localizacion

--- app/src/main/java/pe/edu/ulima/pm/ulgamestore/fragments/ProductsFragment.kt
+++ app/src/main/java/pe/edu/ulima/pm/ulgamestore/fragments/ProductsFragment.kt
@@ -1,14 +1,19 @@
 package pe.edu.ulima.pm.ulgamestore.fragments
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import pe.edu.ulima.pm.ulgamestore.MainActivity
 import pe.edu.ulima.pm.ulgamestore.R
 import pe.edu.ulima.pm.ulgamestore.adapter.ProductsListAdapter
@@ -20,6 +25,8 @@ class ProductsFragment : Fragment() {
         fun onSelect(videogame : Videogame)
     }
     private var listener : OnProductSelectedListener? = null
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -37,6 +44,8 @@ class ProductsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
         ProductsManager(requireActivity().applicationContext).getProductsFirebase({vgList : List<Videogame> ->
             val rviProducts = view.findViewById<RecyclerView>(R.id.rviProducts)
             rviProducts.adapter = ProductsListAdapter(
@@ -50,6 +59,41 @@ class ProductsFragment : Fragment() {
             Toast.makeText(activity, "Error: " + error, Toast.LENGTH_SHORT).show()
         })
 
-
+        getLastLocation()
     }
+
+    private fun getLastLocation() {
+        val permission = ContextCompat.checkSelfPermission(
+            requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // No hay permisos
+            val noPodemosPedirPermisos = ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            if (noPodemosPedirPermisos) {
+                Toast.makeText(requireActivity(),
+                    "Habilitar permisos manualmente", Toast.LENGTH_SHORT).show()
+            }else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION ,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    100
+                )
+            }
+        }else {
+            // Ya tenemos permisos, podemos obtener localizacion
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+                Log.d("ProductsFragment", "${it.latitude} , ${it.longitude}")
+            }
+            fusedLocationClient.lastLocation.addOnFailureListener {
+                Log.e("ProductsFragment", it.message!!)
+            }
+        }
+    }
+
+
 }
